a42b111e4c823f2b64eb3750d97415e8b8d6dbb2
Thomas <thomas@lekoala.be>
2026-09-15 13:33:44 +0200

    add missing getInputStream tests

--- tests/CsvSupportTest.php
+++ tests/CsvSupportTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LeKoala\Baresheet\Tests;
 
+use LeKoala\Baresheet\Exception\InvalidDocumentException;
 use LeKoala\Baresheet\Exception\WriteException;
 use LeKoala\Baresheet\Internal\CsvSupport;
 
@@ -43,5 +44,47 @@ class CsvSupportTest extends TestCase
         // Ensure that something is printed to the output buffer
         // Note: php://output sends to the current output buffer
         $this->assertStringContainsString('default_output', $output);
+    }
+
+    public function testGetInputStreamValidFile(): void
+    {
+        $tempFile = $this->tempFile('csv');
+        file_put_contents($tempFile, 'test_input');
+
+        try {
+            $stream = CsvSupport::getInputStream($tempFile);
+            $this->assertIsResource($stream);
+            $this->assertSame('test_input', stream_get_contents($stream));
+            fclose($stream);
+        } finally {
+            unlink($tempFile);
+        }
+    }
+
+    public function testGetInputStreamInvalidFile(): void
+    {
+        $this->expectException(InvalidDocumentException::class);
+        $this->expectExceptionMessage('Failed to open stream');
+
+        CsvSupport::getInputStream('/invalid/path/that/does/not/exist/file.csv');
+    }
+
+    public function testGetInputStreamRejectsUnsafePath(): void
+    {
+        $this->expectException(InvalidDocumentException::class);
+        $this->expectExceptionMessage('not allowed');
+
+        CsvSupport::getInputStream('phar://archive.phar/file.csv');
+    }
+
+    public function testGetMaxMemTempStreamIsReadWritable(): void
+    {
+        $stream = CsvSupport::getMaxMemTempStream();
+
+        $this->assertIsResource($stream);
+        fwrite($stream, 'buffered');
+        rewind($stream);
+        $this->assertSame('buffered', stream_get_contents($stream));
+        fclose($stream);
     }
 }
